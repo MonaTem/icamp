1dde05ded9bddf463c6f0ab1cdeb3794bfa8c99f
Mona Temchin <monatemchin@gmail.com>
2019-03-30 17:40:31 -0400

    Added test cases, corrected to test for null input

--- reverseWordsInSentencesV2.js
+++ reverseWordsInSentencesV2.js
@@ -1,20 +1,49 @@
 const reverseSentence = (str) => {
   let reversed = "";
-  let previousSpace = str.length - 1;
-  for (i = str.length-1; i >= 0; i--) {
-    if (str[i] === " ") {
-      let sliced = str.slice(i + 1, previousSpace+1);
-      console.log(sliced);
-      reversed = reversed + sliced + " ";
-      previousSpace = i;
-    }
-    if (i === 0) {
-      let sliced = str.slice(0, previousSpace);
-      reversed = reversed + sliced;
-    }
+  if (str && str != null) {
+    let previousSpace = str.length - 1;
+    let sliced = "";
+    for (i = str.length-1; i >= 0; i--) {
+       if (str[i] === " ") {
+         sliced = str.slice(i + 1, previousSpace+1);
+         reversed = reversed + sliced + " ";
+         previousSpace = i;
+       }
+       if (i === 0) {
+         if (previousSpace === str.length - 1) {
+           sliced = str.slice(0, previousSpace + 1);
+         } else sliced = str.slice(0, previousSpace);
+         reversed = reversed + sliced;
+       }
+   }
   }
-return reversed;
+  return reversed;
 }
+/* Test cases:
+Regular case:  A sentence with several words separated by spaces.
 
+Base case or corner case:  1 word sentence
 
-reverseSentence("I live in an apartment");
+Corner case: empty string;
+Corner case: a string with several spaces in it:
+Corner case: a string with one space in it;
+Sanity check: Pass in nothing;
+*/
+//Regular case: a sentence with several words:
+// const str = "I live in an apartment";
+// Base case: 1 word sentence
+// const str = "Yes"
+// corner case: empty string
+// const str = "";
+// corner case: string with one space:
+// const str = " ";
+// corner case:  string with several spaces:
+// const str = "   ";
+// corner case: null string;
+// corner case: sentence begin with space:
+const str = " The rain in Spain falls mainly on the plain";
+// corner case: sentence ends with space:
+// const str = "Ask not what your country can do for you "
+// corner case: pass in nothing:
+// reverseSentence();
+reverseSentence(str);
