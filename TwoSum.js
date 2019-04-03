e748bfe963a50ec3d638fcf13e21e2569198e9fa
Mona Temchin <monatemchin@gmail.com>
2019-04-03 01:26:38 -0400

    Corrected code to add arrow function,  test sum for null, added test cases

--- TwoSum.js
+++ TwoSum.js
@@ -1,13 +1,13 @@
 /* Two Sum Solved by Traversal from Both Ends */
+const twoSum = (arr, sum) => {
+  let start = 0;
 
-  let index1 = 0;
-  let index2 = 0;
   if (arr && arr != null) {
     let end = arr.length - 1;
-    while (start < end) {
+    while (start < end && sum) {
        if (arr[start] + arr[end] === sum) {
-         index1 = arr[start];
-         index2 = arr[end];
+         let index1 = arr[start];
+         let index2 = arr[end];
          return {index1: index1, index2: index2};
        } else if (arr[start] + arr[end] < sum) {
          start++;
@@ -20,4 +20,30 @@
   return null;
 }
 
-twoSum([1, 3, 4, 6, 7, 8, 9, 10], 15);
+// twoSum([1, 3, 4, 5, 6, 7, 8, 9, 10], 15);
+
+// Test Cases
+// Empty array without sum:
+// twoSum([]);
+// Empty array with sum:
+// twoSum([], 3);
+// Single element without sum:
+// twoSum([77]);
+// Single element with sum:
+// twoSum([77], 77);
+// Base case: 2 elements with sum:
+// twoSum([2, 7], 9);
+// Base case: 2 elements with sum, but they don't equal sum
+// twoSum([2, 7], 10);
+// Regular case: 3 elements with sum:
+// twoSum([1, 33, 55], 88);
+// Regular case: 3 elements, but none equal sum
+// twoSum([1, 33, 55], 77);
+// Regular case: 3 elements, no sum
+// twoSum([1, 33, 55]);
+// Regular case: 5 elements, sum
+// twoSum([1, 33, 27, 55, 81], 88);
+// Regular case: 5 elements, no sum
+// twoSum([1, 27, 33, 55, 88]);
+// Regular case: 5 elements, sum, duplicates:
+twoSum([1, 3, 5, 22, 24], 27);
