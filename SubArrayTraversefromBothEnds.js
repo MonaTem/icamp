2d3124de8db68ebe604185e758819e4dc272e575
Mona Temchin <monatemchin@gmail.com>
2019-04-09 21:59:25 -0400

    Added check for null or empty array, additional test cases

--- SubArrayTraversefromBothEnds.js
+++ SubArrayTraversefromBothEnds.js
@@ -3,22 +3,25 @@ subArrayTraversefromBothEnds = (arr) => {
   let j = 0;
   let subArray = [];
 
+ if (!arr) return null;
 
-  for (i = 0; i <= arr.length - 1; i++) {
-    if (arr[i+1] < arr[i]) {
-       break;
-    }
+ for (i = 0; i <= arr.length - 1; i++) {
+   if (arr[i+1] < arr[i]) {
+      break;
+   }
+ }
+
+
+ for (j = arr.length - 1; j >= 0; j--) {
+  if (arr[j -1]  > arr[j]) {
+     break;
   }
+ }
 
-
-  for (j = arr.length - 1; j >= 0; j--) {
-    if (arr[j -1]  > arr[j]) {
-       break;
-    }
-  }
 
 
   // no dip or bump
+
   if (i >= arr.length - 1 && j <= 0) {
     return null;
   }
@@ -60,4 +63,24 @@ subArrayTraversefromBothEnds = (arr) => {
 
 // Regular case: sorted portion at end:
 
-subArrayTraversefromBothEnds([3, 1, 5, 2, 4, 6, 7, 8, 9])
+// subArrayTraversefromBothEnds([3,1,5,2,4,6,7,8,9])
+
+// Edge case: null array
+
+// subArrayTraversefromBothEnds(null);
+
+// Edge case: empty array
+
+// subArrayTraversefromBothEnds([]);
+
+// Base case: one element:
+
+// subArrayTraversefromBothEnds([7]);
+
+// Base case: two elements, unsorted:
+
+// subArrayTraversefromBothEnds([88, 52]);
+
+// Base case: two elements, sorted:
+
+subArrayTraversefromBothEnds([77, 903]);
