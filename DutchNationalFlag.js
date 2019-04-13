b78f745436d25f5e63884fec3b61ca6672aa32ba
Mona Temchin <monatemchin@gmail.com>
2019-04-13 17:09:26 -0400

    Added test cases to Dutch National Flag

--- DutchNationalFlag.js
+++ DutchNationalFlag.js
@@ -53,7 +53,47 @@ const dutchNationalFlag = (a, X) => {
 return a;
 }
 
-// Regular case
+// Regular case..multiple elements in pivot
 // Expected result:  [3, 2, 3, 4, 4, 4, 4, 5, 6, 8, 6]
 
-dutchNationalFlag([3, 5, 2, 6, 8, 4, 4, 6, 4, 4, 3], 5)
+// dutchNationalFlag([3, 5, 2, 6, 8, 4, 4, 6, 4, 4, 3], 5)
+
+/* Test cases:
+Regular case: single element in pivot, duplicate pivots, array with no elements less than pivot, array with no elements > pivot
+Base case: single element less, equal greater; 2 elements less equal greater
+Edge cases: null array, X < 0, X > array length, empty array, array with 1 element
+Sanity check: pass in nothing
+*/
+
+// Regular case: 1 pivot element
+//  dutchNationalFlag([1, 3, 55, 7, 89, 2], 2);
+
+// Regular case: array with no elements < pivot
+// dutchNationalFlag([99, 88, 44, 11, 101], 3);
+
+// Regular case: array with no elements > pivot
+// dutchNationalFlag([ 55, 66, 77, 27, 8], 2);
+
+// Base case: single element less than, greater than, equal
+// dutchNationalFlag([222, 2700, 1013], 2);
+
+// Base case: two elements less than, greater than, equal
+// dutchNationalFlag([333, 4406, 17, 2501, 8899, 2501], 3)
+
+// Edge case: null array
+// dutchNationalFlag(null, 0);
+
+// Edge case: empty array
+// dutchNationalFlag([], 0);
+
+// Edge case: array with 1 element:
+// dutchNationalFlag([7], 0);
+
+// Edge case: X < 0
+// dutchNationalFlag([2, 5, 8], -3);
+
+// Edge case: X > array.length
+//
+
+// Sanity check: pass in nothing
+dutchNationalFlag();
