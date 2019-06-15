44ee761cc060f17ea2cb7661f3627b4ac2bb2bd5
Mona Temchin <monatemchin@gmail.com>
2019-06-14 22:48:29 -0400

    For FindWithUnknownLength, correct comments, comment out console.log statements

--- FindWithUnknownLength.js
+++ FindWithUnknownLength.js
@@ -1,8 +1,9 @@
+
 /* Problem description:
 
 Find the target element in an array of unknown length.
 
-Solution: To find the array's length, start with high of  1 and keep doubling it, until the element at the index is invalid and therefore throws an exception.
+Solution: To find the array's length, start with high of 1 and keep doubling it until the index is invalid and therefore throws an exception.
 
 So then we know that the value exists between high/2 (the last value of index before we doubled it) and the high point (the current value of the index which threw the exception).
 
@@ -26,19 +27,19 @@ Functions:
 
 There will be 3 functions:
 
-The main one will findwithUnknownLength.
+The main one will be findwithUnknownLength.
 
-The first helper function will binarySearchforLastIndex.
+The first helper function will be binarySearchforLastIndex.
 
-The second helper function will binarySeachWithinRange.
+The second helper function will be binarySeachWithinRange.
 
 Pseudocode:
 
 If array or target empty or null or nothing passed in, return -1.
 
-i = 1;
+high = 1;
 
-Multiply i by 2.
+Multiply high by 2.
 keep checking until  a[i] is undefined (use a try/catch block to check this);
 
 Then...do a binary search for target in range defined by high = a[i] where undefined and low = high/2.
@@ -57,10 +58,12 @@ Edge cases:  nothing passed in, aray null or undefined or empty, target null or 
 */
 
 const binarySearchforLastIndex = (a, low, high) => {
-
+  // console.log('binarySearchforLastIndex');
+  // console.log(`low is ${low}`);
+  // console.log(`high is ${high}`);
   while (low <= high) {
      let mid = Math.floor(low + (high - low)/2);
-
+    //  console.log(`here mid is ${mid}`);
      try {
       let temp = a[mid];
       if (!temp)
@@ -76,20 +79,31 @@ const binarySearchforLastIndex = (a, low, high) => {
         throw new Error("array index is out of bounds");
      }
      catch(error) { // mid + 1 is out of bounds.  mid is last index
-
+      //  console.log("we are in mid+1 out of bounds");
        return mid;
      }
-     low = mid + 1; //both mid and mid + 1 are in array,                   so mid  is not the last index
+     low = mid + 1; //both mid and mid + 1 are in array,  so mid  is not the last index
   }
   return -1;  // this array has no last index
 };
 
 
 const binarySearchWithinRange = (a, target, low, high) => {
-
+  // console.log(a, target, low, high);
   while (low < high) {
      let mid = Math.floor(low + (high - low)/2);
-
+    //  console.log(`mid is now ${mid}`);
+    //  console.log`low and high are now ${low} ${high}`;
+    //  if (a[mid] === target) return mid;
+    //  else if
+    //   high = mid - 1;
+    // } else if (a[mid] < target) {
+    //        low = mid + 1;
+    //        console.log(`new low is ${low}`);
+    //        } else {
+    //          console.log(`here we are!!!!`);
+    //          return mid;
+    //          }
     if (a[mid] > target) {
       high = mid - 1;
     } else if (a[mid] < target) {
@@ -117,11 +131,10 @@ const findWithUnknownLength = (a, target) => {
     }
   high +=2;
   }
-
+  // console.log(`low is  ${Math.floor(high/2)}`);
   return binarySearchWithinRange(a, target, Math.floor(high/2), high);
 };
-
- findWithUnknownLength([-202, -101, 2, 4, 27, 31, 55, 237, 345, 2020, 6780, 52078], 2020);
+findWithUnknownLength([-202, -101, 2, 4, 27, 31, 55, 237, 345, 2020, 6780, 52078], 2020);
 
 // findWithUnknownLength([-202, -101, 2, 4, 27, 31, 55, 237, 345, 2020, 6780, 52078], 2023);
 
