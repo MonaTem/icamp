fd1dd1c7e807a7bc43111e12a489d6089d2f85cb
Mona Temchin <monatemchin@gmail.com>
2019-06-27 20:49:25 -0400

    Coded Fibonacci Numbers with Recursion and Memoization

---
+++ FibonacciWithMemo.js
@@ -0,0 +1,11 @@
+// Fibonacci numbers using recursion and memoization
+const FibonacciWithMemo = (n, map) =>
+{ if (n === 1 || n === 2) return 1;
+  if (map.has(n)) return map.get(n);
+  let result = FibonacciWithMemo(n-1, map)
+             + FibonacciWithMemo(n-2, map);
+  map.set(n, result);
+  return result;
+}
+var map = new Map();
+FibonacciWithMemo(20, map);
