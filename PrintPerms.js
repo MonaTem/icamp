4bf5a158c7697ad490f7a3ba669aca5d0ee65d53
Mona Temchin <monatemchin@gmail.com>
2019-09-15 17:50:49 -0400

    Added initial comments--problem desc, solution, pseudocode, test case desc, etc.--to Print Permutations

---
+++ PrintPerms.js
@@ -0,0 +1,50 @@
+/* Problem description:
+   Given array a, print all permutations of size x.
+
+   Questions and answers:
+
+   Q.  Will there be any duplicates in the input array?
+   A.  No.
+
+   Q.  If the input is an empty array or just empty,
+       should we throw an exception or just print nothing?
+   A.  Print nothing.
+
+   Q.  If x is bigger than the length of a, what to do?
+   A.  Print nothing
+
+   Solution:
+
+   Place candidates in the buffer and print the buffer when it is full.
+
+   Use a recursive helper function to find candidates.
+   Keep track of whether the candidate digit is already in the array.
+
+   Pseudocode:
+
+   PrintPermsHelper(a, buffer, bufferIndex, isInBuffer)
+
+   If buffer is full, print buffer & return.
+
+   for i from 0 to length of a:
+      if isInBuffer[i] is false, place a[i] in buffer[bufferIndex]
+      set isInBuffer[i] to true
+      // recurse to next slot in buffer
+      PrintPermsHelper(a, buffer, bufferIndex+1, isInBuffer)
+      set isInBuffer[i] to false
+
+
+   Test cases:
+
+   Regular case:  x less than or equal to length of a
+
+   Base cases: a length 1 or 2, x is 1
+
+   Edge cases: a empty, or empty array or null; x undefined
+
+   Big O:
+
+   Time complexity: exponential O(2^n)
+   Space complexity: O(X) for both buffer allocation and recursion stack
+
+*/
