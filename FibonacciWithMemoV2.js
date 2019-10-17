1cc065c29530257c1698f3d7f9d7e02adb4078db
Mona Temchin <monatemchin@gmail.com>
2019-10-17 16:25:16 -0400

    Alternate Version Fibonacci with Memoization and Recursion

---
+++ FibonacciWithMemoV2.js
@@ -0,0 +1,12 @@
+const fibonacci = (n,memo) => {
+    memo = memo || {}
+    if (memo[n]) {
+        return memo[n]
+    }
+    if (n <= 2) {
+        return 1
+    }
+    return memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo)
+};
+
+fibonacci(8);
